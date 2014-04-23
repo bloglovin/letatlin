53a81b14b432d55bf5c8bc64de94de13be9784d6
Hugo Wetterberg <hugo@wetterberg.nu>
2014-04-23 17:42:28 -0400

    Persisting environment config so that it can be used as a fallback if the connection to etcd fails at a later date

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 var lib = {
   url: require('url'),
+  fs: require('fs'),
   path: require('path'),
   async: require('async'),
   minimist: require('minimist'),
@@ -13,7 +14,17 @@ var lib = {
 
 var internal = {};
 
-function letatlin(env, callback) {
+function letatlin(env, options, callback) {
+  if (typeof options == 'function') {
+    callback = options;
+    options = {};
+  }
+
+  options.persistConfig = options.persistConfig === undefined ? true : options.persistConfig;
+  options.persistPath = options.persistPath || './environment.config.json';
+
+  // The raw key-values from etcd
+  var raw = {};
   // Get information on the application that's hosting us
   // We check for an etcd-flag that tells us to connect to a non-local etcd.
   var args = lib.minimist(process.argv.slice(2));
@@ -25,27 +36,28 @@ function letatlin(env, callback) {
   var etcd = new lib.Etcd(url.hostname, url.port || 4001);
 
   // Worker function for our config fetcher
-  function getConfig(info, callback) {
+  function getConfig(info, done) {
     etcd.get(info.key, {
       recursive: !!info.recursive
     }, function getResult(error, result) {
-      if (error) return callback(error);
+      if (error) return done(error);
 
       var value;
 
       if (result.node.dir) {
         // Recursively map etcd nodes in a dir to an object.
-        value = internal.dirToObject(result.node);
+        value = internal.dirToObject(result.node, raw);
+        raw[result.node.key] = value;
       }
       else {
-        value = result.node.value;
-        // If a format has been specified we try to parse the
+        raw[result.node.key] = value = result.node.value;
+        // If a format has been specified we try to parse the value.
         if (value !== undefined && info.format && lib.formats[info.format]) {
           value = lib.formats[info.format](value);
         }
       }
 
-      callback(null, value);
+      done(null, value);
     });
   }
 
@@ -54,8 +66,57 @@ function letatlin(env, callback) {
   for (var name in env) {
     tasks[name] = lib.async.apply(getConfig, env[name]);
   }
-  lib.async.series(tasks, callback);
+  lib.async.series(tasks, function loadResult(error, values) {
+    if (error) {
+      internal.storedConfigFallback(error, options, env, callback);
+      return;
+    }
+
+    // Persist the raw config if we're configured to do so.
+    if (options.persistConfig) {
+      lib.fs.writeFile(options.persistPath,
+        JSON.stringify(raw, null, '  '),
+        {encoding:'utf8'},
+        function persistResult(error) {
+          if (error) {
+            console.error('Failed to persist configuration', error);
+          }
+        }
+      );
+    }
+
+    callback(null, values);
+  });
 }
+
+// If we fail to read config off etcd we still want to start the application
+// if we have a config on file. This might happen if the process i restarted.
+internal.storedConfigFallback = function (error, options, env, callback) {
+  if (!options.persistConfig) return callback(error);
+
+  lib.fs.readFile(options.persistPath,
+    {encoding:'utf8'},
+    function readResult(readError, data) {
+      if (readError) return callback(error);
+
+      try {
+        var config = {};
+        var rawConfig = JSON.parse(data);
+        for (var name in env) {
+          var info = env[name];
+          var value = rawConfig[info.key];
+          // If a format has been specified we try to parse the value.
+          if (value !== undefined && info.format && lib.formats[info.format]) {
+            value = lib.formats[info.format](value);
+          }
+          config[name] = value;
+        }
+        callback(null, config);
+      } catch (transformError) {
+        callback(transformError);
+      }
+    });
+};
 
 // Recursively map etcd nodes in a dir to an object.
 internal.dirToObject = function dirToObject(dir) {
