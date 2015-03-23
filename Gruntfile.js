bc39e10a5ff7598ea876487a58b9e07c6d897a75
Pelle Wessman <pelle@kodfabrik.se>
2015-03-23 14:55:01 +0100

    test(main): restoring dependency check
    As upstream issue has been fixed

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,5 @@ var lintlovin = require('lintlovin');
 module.exports = function (grunt) {
   lintlovin.initConfig(grunt, {}, {
     noJSCS: true,
-    // Opting out of this due to: https://github.com/juliangruber/builtins/pull/6
-    noDependencyCheck: true,
   });
 };
